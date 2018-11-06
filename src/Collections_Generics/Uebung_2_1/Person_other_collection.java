19108169305d8ba156de1fa8a58a8eb3deeb6831
Jan Lindheimer <81lia1bif@hft-stuttgart.de>
2018-11-06 14:00:04 +0100

    Update

--- src/Collections_Generics/Uebung_2_1/Person_other_collection.java
+++ src/Collections_Generics/Uebung_2_1/Person_other_collection.java
@@ -1,4 +1,28 @@
 package Collections_Generics.Uebung_2_1;
 
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Person_other_collection {
+
+    public static void main(String[] args) {
+
+        SortedSet<Person> set = new TreeSet<>();
+
+        set.add(new Person("Heusch","Peter", 5));
+        set.add(new Person("Rausch","Aexander", 1));
+        set.add(new Person("pado","Ulrike", 4));
+        set.add(new Person("Kanuth","Stefan", 2));
+        set.add(new Person("mosler","Christoph", 3));
+
+        //Iterating over the persons in the set
+        Iterator it = set.iterator();
+
+        while(it.hasNext()){
+            Person pers = (Person)it.next();
+            System.out.println(pers);
+        }
+    }
+
 }
